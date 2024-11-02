8759142c9e220d02c5880cb2a178376ffd6ef038
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-11-02 10:05:40 +0100

    changed test GET

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@ import test from "node:test";
 import request from "supertest";
 import { createApp } from "./app";
 
-test("GET /", async () => {
+test("GET / should return status 200", async () => {
   const app = createApp();
   const response = await request(app).get("/");
   deepEqual(response.status, 200);
