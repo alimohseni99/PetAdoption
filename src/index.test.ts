1879ec85d60ffa0b6fd91673a445c5d3f33d1f46
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-11-02 22:55:27 +0100

    added jest for testing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,11 @@
 import { deepEqual } from "node:assert/strict";
-import test from "node:test";
 import request from "supertest";
 import { createApp } from "./app";
 
-test("GET / should return status 200", async () => {
-  const app = createApp();
-  const response = await request(app).get("/");
-  deepEqual(response.status, 200);
+describe("GET /", () => {
+  it("should return status 200", async () => {
+    const app = createApp();
+    const response = await request(app).get("/");
+    deepEqual(response.status, 200);
+  });
 });
