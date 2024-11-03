959e47aec3f1eb2c68441fb130ee04ffc3c9126a
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-11-03 23:36:27 +0100

    moved out createPetDb from app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { createAdoptionModule } from "./modules";
 import { AdoptionData, Db } from "./modules/adoption-feature/types";
 import { createPetModule } from "./modules/pet-feature";
-import { PetDb, PetType } from "./modules/pet-feature/types";
+import { createPetDb } from "./modules/pet-feature/create-db";
 function createDb(): Db {
   const data: AdoptionData[] = [];
   return {
@@ -40,44 +40,6 @@ function createDb(): Db {
   };
 }
 
-function createPetDb(): PetDb {
-  const data: PetType[] = [];
-  return {
-    pets: {
-      getAllPets: async () => data,
-      create: async (PetInfo: PetType): Promise<PetType> => {
-        data.push(PetInfo);
-        return PetInfo;
-      },
-      deletePet: async (id: string): Promise<void> => {
-        const index = data.findIndex((pet) => pet.id === id);
-        if (index === -1) {
-          throw new Error(`Pet with id ${id} not found`);
-        }
-        data.splice(index, 1);
-      },
-      getPetById: async (id: string): Promise<PetType> => {
-        const pet = data.find((pet) => pet.id === id);
-        if (!pet) {
-          throw new Error(`pet with id ${id} not found`);
-        }
-        return pet;
-      },
-      updatePet: async (
-        id: string,
-        petData: Partial<PetType>
-      ): Promise<PetType> => {
-        const index = data.findIndex((pet) => pet.id === id);
-
-        if (index === -1) {
-          throw new Error(`pet with id ${id} not found`);
-        }
-        data[index] = { ...data[index], ...petData };
-        return data[index];
-      },
-    },
-  };
-}
 export function createApp() {
   const app = express();
   app.use(express.json());
